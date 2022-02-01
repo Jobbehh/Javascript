6d45d42b1ae6df5adf03aae28300be6fd5e730fc
Jobbehh <job_foster@hotmail.com>
2022-02-01 15:21:31 +0000

    added fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+`use strict`;
+
+for (let a = 1; a < 101; a++){
+    if(a % 15 ==0 )
+    console.log("FizzBuzz!");
+else if (a % 3 == 0){
+console.log("Fizz!");}
+else if (a % 5 ==0){
+    console.log("Buzz!");
+} else console.log(a)
+}
+
