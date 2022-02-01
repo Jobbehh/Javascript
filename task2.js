1b03af88cc5266961c86bf1c7c1f022844637e6c
Jobbehh <job_foster@hotmail.com>
2022-02-01 11:48:51 +0000

    added task 2

---
+++ task2.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+
+typeof(a);
+typeof(b);
+typeof(c);
+typeof(d);
+typeof(e);
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+`The total bill is £4000 the remaining amount of money to be paid is £1652`
+
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
