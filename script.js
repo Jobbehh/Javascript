8f403f2e7074e4d757f9daeee075add5be908c1f
Jobbehh <job_foster@hotmail.com>
2022-02-01 10:31:55 +0000

    added task1

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let a = 2;
+
+console.log("hello World!");
+
+let text = 'this is a message'
+
+console.log(text);
+
+console.info(text);
+
+console.warn(text);
+
+console.error(text);
+
+console.log('%c', + text,'colour:black; background-color:white;font-size: 20px; paddding 10px');
+
+console.log('%c' + text +_,)
---
+++ task _1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let fname ="Job"
+let lname= "Foster"
+let loc= "London"
+let Starsign= "taurus but they arent real"
+
+console.log(fname);
+
+console.info(lname);
+
+console.warn(loc);
+
+console.error(Starsign);
+
+console.log("%c Hello my name is Job Foster from london and star signs arent real","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
